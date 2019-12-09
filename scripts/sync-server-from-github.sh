5dbafeacb3400630d3fbfa3862f975d9b042ff04
Conrad Sollitt <57777521+ConradSollitt@users.noreply.github.com>
2019-12-09 12:36:10 -0800

    :robot: Sync script updated for typo

--- scripts/sync-server-from-github.sh
+++ scripts/sync-server-from-github.sh
@@ -6,7 +6,7 @@
 #  and is used to sync the latest changes from GitHub. It runs manually from
 #  the author once published changes are confirmed.
 #
-#  Only site templates are sync, any other code changes require manual updates.
+#  Only site templates are synced, any other code changes require manual updates.
 #  This is due to the fact that the playground server uses a hard-coded
 #  security key for authentication which only exists on the server. A 
 #  hard-coded key is used over a [.env] file for performance (to avoid loading
